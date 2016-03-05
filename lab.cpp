f90e40abd470775fb1b8ad7278fd17d551733557
aleksgorovenko <aleksgorovenko@gmail.com>
2016-03-05 16:03:14 +0600

    decision and output

--- lab.cpp
+++ lab.cpp
@@ -1,5 +1,5 @@
-#includestdio.h
-#includemath.h
+#include<stdio.h>
+#include<math.h>
 
 int main()
 {
@@ -7,12 +7,22 @@ int main()
 	float d = 0.0;
 	float x1 = 0.0, x2 = 0.0;
 	
-	printf(Input a );
-	scanf(%f, &a);
-	printf(Input b );
-	scanf(%f, &b);
-	printf(Input c );
-	scanf(%f, &c);
+	printf("Input a: ");
+	scanf("%f", &a);
+	printf("Input b: ");
+	scanf("%f", &b);
+	printf("Input c: ");
+	scanf("%f", &c);
+	
+	d = (b*b) - (4*a*c);
+	if(d >= 0)
+	{
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+	
+		printf("First answer: %f\nSecond anwer: %f", x1, x2);
+	}
+	else printf("discriminant < 0");
 	
 	return 0;
 }
