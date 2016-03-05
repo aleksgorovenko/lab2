35f773f36e647d7c38045b2190728c6095c49b6b
aleksgorovenko <aleksgorovenko@gmail.com>
2016-03-05 16:00:08 +0600

    input a,b,c

--- lab.cpp
+++ lab.cpp
@@ -3,5 +3,16 @@
 
 int main()
 {
+	float a = 0.0, b = 0.0, c = 0.0;
+	float d = 0.0;
+	float x1 = 0.0, x2 = 0.0;
+	
+	printf(Input a );
+	scanf(%f, &a);
+	printf(Input b );
+	scanf(%f, &b);
+	printf(Input c );
+	scanf(%f, &c);
+	
 	return 0;
 }
